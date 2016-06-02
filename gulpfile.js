ee03c71c520ce72d353ab741bd3b864c34123f0e
Siggi Duenkel <chaosprinz76@googlemail.com>
2016-06-02 12:42:27 +0200

    Proper function naming, refactoring and documentation

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ const Stylus = require('gulp-stylus')
 const Sourcemaps = require('gulp-sourcemaps')
 const Join = require('path').join
 
+/**
+ * ## readConfigFile
+ * require given file or the default-config-file and read in the stylus-conf
+ * @param  {[string]} file path to the config-file to use
+ * @return {[Object]}      stylus config-object
+ */
 let readConfigFile = function (file) {
   let module
   try {
@@ -15,6 +21,11 @@ let readConfigFile = function (file) {
   return module.stylus
 }
 
+/**
+ * ## buildPathes
+ * @param  {[Array]} pathes Elements are file-glob-arrays
+ * @return {[Array]}        paths/file-globs as strings with prefixed cwd
+ */
 let buildPathes = function (pathes) {
   pathes = pathes.map(path => {
     return Join(process.cwd(), ...path)
@@ -22,17 +33,35 @@ let buildPathes = function (pathes) {
   return pathes
 }
 
-let setLibs = function (cfg) {
+/**
+ * ## setStylusLibs
+ * @param {[Object]} cfg     new config-object
+ * @return {[Object]}        config-object with proper Lib-setup on stylus
+ *                           use-option
+*/
+let setStylusLibs = function (cfg) {
   let conf = cfg
-  console.log(conf)
   conf.conf.use = cfg.conf.use.map(lib => {
     return require(lib)()
   })
   return conf
 }
 
-let setup = function (cfg) {
-  let conf = setLibs(cfg)
+/**
+ * ## setupStylusConf
+ * use cfg-object to setup a proper stylus-config
+ * @param  {[Object]} cfg
+ *   An Object with following keys:
+ *   	 - main: array of glob-arrays, use by gulp.src
+ *   	 - includes: array of glob-arrays, additional used for stylus-import
+ *     - dest: array with an path-array-element for gulp.dest
+ *     - conf: the config passed to stylus
+ * @return {[Object]}     object with proper stylus-config
+ */
+let setupStylusConf = function (cfg, devMode) {
+  devMode = devMode || true
+  let conf = setStylusLibs(cfg)
+  conf.conf.compress = devMode ? false : true
   let components = buildPathes(conf.includes)
   let modules = conf.conf.import
 
@@ -44,25 +73,27 @@ let setup = function (cfg) {
   }
 }
 
-let build = function (cfg, dev) {
-  dev = dev || true
+/**
+ * ## build
+ * use cfg-object to setup a proper gulp-pipeline and return it on
+ * gulp.dest-pipe
+ * @param  {[Object]} cfg     Object with a stylus-key, used for setupStylusConf()
+ * @param  {[Bool]} devMode   false when build is for production
+ * @return {gulp-pipeline}  returns the .pipe(gulp.dest())-call of the pipeline
+ */
+let build = function (cfg, devMode) {
+  devMode = devMode || true
 
-  let conf = setup(cfg)
+  let conf = setupStylusConf(cfg)
   let runner = gulp.src(conf.src)
-
-  if (dev) {
-    runner = runner.pipe(Sourcemaps.init())
-    conf.stylusConf.compress = false
-  }
-
+  runner = devMode ? runner.pipe(Sourcemaps.init()) : runner
   runner = runner.pipe(Stylus(conf.stylusConf))
-  runner = dev ? runner.pipe(Sourcemaps.write('.')) : runner
+  runner = devMode ? runner.pipe(Sourcemaps.write('.')) : runner
 
   return runner.pipe(gulp.dest(conf.dest))
 }
 
 let cfg = readConfigFile(Join(process.cwd(), 'config', 'build.conf.json'))
-
 gulp.task('stylus', function () {
   build(cfg)
 })
