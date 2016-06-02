28554e77be4c85d3bc4cfa02c7a5532f21418069
Siggi Duenkel <chaosprinz76@googlemail.com>
2016-06-02 11:24:45 +0200

    Dry things up by using cfg-file + a more functinal approach

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,56 @@ const gulp = require('gulp')
 const Stylus = require('gulp-stylus')
 const Sourcemaps = require('gulp-sourcemaps')
 const Join = require('path').join
-const Nib = require('nib')
-const Jeet = require('jeet')
-const Rupture = require('rupture')
+const Cfg = require(Join(process.cwd(), 'config', 'build.conf.json')).stylus
 
-let stylusSheets = Join(process.cwd(), 'src', 'stylesheets', '**/*.styl')
-let components = Join(process.cwd(), 'src', 'components', '**/*.styl')
-let destination = Join(process.cwd(), 'dist', 'public', 'css')
+
+let buildPathes = function (pathes) {
+  pathes = pathes.map(path => {
+    return Join(process.cwd(), ...path)
+  })
+  return pathes
+}
+
+let setLibs = function (cfg) {
+  let conf = cfg
+  conf.conf.use = cfg.conf.use.map(lib => {
+    return require(lib)()
+  })
+  return conf
+}
+
+let setup = function (cfg) {
+  let conf = setLibs(cfg)
+  let components = buildPathes(conf.includes)
+  let modules = conf.conf.import
+
+  conf.conf.import = [...modules, ...components]
+  return {
+    stylusConf: conf.conf,
+    src: buildPathes(conf.main),
+    dest: buildPathes(conf.dest)[0]
+  }
+}
+
+let build = function (cfg, dev) {
+  dev = dev || true
+
+  let conf = setup(cfg)
+  let runner = gulp.src(conf.src)
+
+  if (dev) {
+    runner = runner.pipe(Sourcemaps.init())
+    conf.stylusConf.compress = false
+  }
+
+  runner = runner.pipe(Stylus(conf.stylusConf))
+  runner = dev ? runner.pipe(Sourcemaps.write('.')) : runner
+
+  return runner.pipe(gulp.dest(conf.dest))
+}
 
 gulp.task('stylus', function () {
-  gulp.src([stylusSheets])
-    .pipe(Sourcemaps.init())
-    .pipe(Stylus({
-      use: [Nib(), Rupture(), Jeet()],
-      import: ['nib', 'jeet', components]
-    }))
-    .pipe(Sourcemaps.write('.'))
-    .pipe(gulp.dest(destination))
+  build(Cfg)
 })
 
 module.exports = gulp.tasks
